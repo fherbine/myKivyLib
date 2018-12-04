160781c270906547a2599dc94c863c17525cd17f
fherbine <felix.herbinet@yahoo.com>
2018-12-04 13:12:01 +0100

    First version of the circle layout done

--- own_uix/circlelayout.py
+++ own_uix/circlelayout.py
@@ -9,16 +9,16 @@ from kivy.properties import (
 
 class CircleLayout(Layout):
     orientation = OptionProperty('clock', options=('clock', 'reverse_clock'))
-    center = VariableListProperty([0, 0])
     radius = NumericProperty(0)
     first_widget_pos = NumericProperty(0)
+
 
     def __init__(self, **kwargs):
         """ Circle layout initialization, binding needed on properties """
         super(CircleLayout, self).__init__(**kwargs)
         update  = self._trigger_layout
+        fbind = self.fbind
         fbind('orientation', update)
-        fbind('center', update)
         fbind('radius', update)
         fbind('first_widget_pos', update)
         fbind('children', update)
@@ -26,7 +26,10 @@ class CircleLayout(Layout):
         fbind('size', update)
         fbind('pos', update)
 
-    def do_layout(self):
+    def on_children(self, _, children):
+        self._trigger_layout()
+
+    def do_layout(self, *largs):
         len_children = len(self.children)
         if len_children == 0:
             return
@@ -35,12 +38,27 @@ class CircleLayout(Layout):
             return
 
         first_pos = first_pos / 180.0 * math.pi
-        offset = 2.0 * math.pi / len_children
         orientation = self.orientation
+        if orientation == 'reverse_clock':
+            offset_angle = 2.0 * math.pi / float(len_children)
+        else:
+            offset_angle = -2.0 * math.pi / float(len_children)
+
+        for index, child in enumerate(reversed(self.children)):
+            child.pos = [
+                self.center[index] + (xy * self.radius)
+                for index, xy in enumerate((
+                    math.cos(offset_angle * index),
+                    math.sin(offset_angle * index)
+                ))
+            ]
+
 
 
     def add_widget(self, widget, index=0):
-        pass
+        widget.bind(pos_hint=self._trigger_layout)
+        return super(CircleLayout, self).add_widget(widget, index) 
 
     def remove_widget(self, widget):
-        pass
+        widget.bind(pos_hint=self._trigger_layout)
+        return super(CircleLayout, self).remove_widget(widget)
